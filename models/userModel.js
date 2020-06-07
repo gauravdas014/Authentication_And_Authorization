7bd1a2b1122ca75cd5cca490b1c610479ae18fab
Gaurav Das <gauravdas014@gmail.com>
2020-06-07 14:33:13 +0530

    add validatirs in the user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -7,17 +9,35 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
   },
+  email: {
+    type: String,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
   password: {
     type: String,
   },
   passwordConfirm: {
     type: String,
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not same",
+    },
   },
   role: {
     type: String,
     enum: ["student", "teacher", "admin"],
     default: "student",
   },
+});
+
+userSchema.pre("save", async function (next) {
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
