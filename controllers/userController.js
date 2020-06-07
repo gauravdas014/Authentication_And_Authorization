9624c9deb25f8cc34c113b0f14bf74473833a4fa
Gaurav Das <gauravdas014@gmail.com>
2020-06-07 14:12:26 +0530

    implement update tour

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,13 +50,27 @@ exports.createUser = async (req, res) => {
   }
 };
 
-exports.deleteUser = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "This route is not yet implemented",
-  });
+exports.updateUser = async (req, res) => {
+  try {
+    const tour = await User.findByIdAndDelete(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "User details could not be updated",
+    });
+  }
 };
-exports.updateUser = (req, res) => {
+
+exports.deleteUser = (req, res) => {
   res.status(200).json({
     status: "success",
     message: "This route is not yet implemented",
