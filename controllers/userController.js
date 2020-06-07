e933e99d70e464a982688971d7cbf56c3e6bc4aa
Gaurav Das <gauravdas014@gmail.com>
2020-06-07 13:42:55 +0530

    add create new user funcion

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,12 +10,24 @@ exports.getUser = (req, res) => {
     message: "This route is not yet implemented",
   });
 };
-exports.createUser = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "This route is not yet implemented",
-  });
+
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid data sent",
+    });
+  }
 };
+
 exports.deleteUser = (req, res) => {
   res.status(200).json({
     status: "success",
